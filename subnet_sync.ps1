c5b1ba8cfc2d7ce7bee0ec6c5ffe9c0e20b3dec1
mute65 <mevraets65@gmail.com>
2022-01-18 20:41:01 -0700

    cleaned up comments

--- subnet_sync.ps1
+++ subnet_sync.ps1
@@ -1,12 +1,24 @@
-# Import CSV of Scope information and create new scopes w/ failover relationships and add to sites and services.
+<#  Import CSV of Scope information and create new scopes w/ failover relationships and add to sites and services.
+    HEADER ROW OF CSV - ScopeName,StartRange,EndRange,SubnetMask,LeaseDuration,Failover,ScopeId,SiteSubnet,Site,SiteLocation,Router
+    
+    Notes on formatting:
+    ScopeName - Name of scope as viewable in DHCP. Also used for description in Sites and Services
+    StartRange - First usable IP
+    EndRange - Last usable IP
+    SubnetMask - xxx.xxx.xxx.xxx format of mask
+    LeaseDuration - D.HH:MM:SS
+    Failover - Name of preconfigured failover relationship
+    ScopeId - Network identifier
+    SiteSubnet - Network identifier + CIDR (e.g. 10.0.0.0/24)
+    Site - Site code defined in Active Directory
+    SiteLocation - Same as ScopeName (description in Sites and Services)
+    Router - Default gateway for scope options in DHCP #>
 
 $Data = Import-Csv "Path-to-file.csv"
 $DHCPServer = FQDN-OF-DHCP-SERVER
 
 
-# Create scopes (LeaseDuration in day.hrs:mins:secs)
-### NEED TO ADD SCOPE OPTION FOR DEFAULT GATEWAY ###
-### ADD TARGET SERVER ###
+# Create DHCP Scopes
 
 $Data | ForEach-Object {
 
@@ -31,7 +43,6 @@ $Data | ForEach-Object {
 }
 
 # Add scopes to sites and services
-### CHANGE LOCATION TO DESCRIPTION ###
 
 $Data | ForEach-Object {
 
